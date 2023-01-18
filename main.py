788d04975957274cb16883038c6c263365376468
Stepan Bakshayev <step2back@gmail.com>
2023-01-18 21:24:02 +0200

    [0h:43min] give birth docker container

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
+import docker
 import typer
+from devtools import debug
 
 
 def main(
     docker_image: str = typer.Option("python"),
     bash_command: str = typer.Option(
-        r"""$'pip install pip -U && pip
-install tqdm && python -c \"import time\ncounter = 0\nwhile
-True:\n\tprint(counter)\n\tcounter = counter + 1\n\ttime.sleep(0.1)\"'"""
+        r"""pip install pip -U && pip install tqdm && python -u -c "import time
+counter = 0
+while True:
+    print(counter)
+    counter = counter + 1
+    time.sleep(0.1)
+" """
     ),
-    aws_cloudwatch_group: str = typer.Option("test-task-group-1"),
-    aws_cloudwatch_stream: str = typer.Option("test-task-stream-1"),
-    aws_access_key_id: str = typer.Option(...),
-    aws_secret_access_key: str = typer.Option(...),
-    aws_region: str = typer.Option(...),
+    # aws_cloudwatch_group: str = typer.Option("test-task-group-1"),
+    # aws_cloudwatch_stream: str = typer.Option("test-task-stream-1"),
+    # aws_access_key_id: str = typer.Option(...),
+    # aws_secret_access_key: str = typer.Option(...),
+    # aws_region: str = typer.Option(...),
 ):
-    print(f"Hello {{name}}")
+    client = docker.from_env()
+    container = client.containers.run(docker_image, ["sh", "-c", bash_command], detach=True)
+    debug(container, container.id, container.logs())
+    import time
+
+    time.sleep(5)
+    debug(container, container.id, container.logs())
+    container.stop()
 
 
 if __name__ == "__main__":
